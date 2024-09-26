9c268cca33b04d6441368390cc20dfcc3641388d
enriqueferretiz <kikeferretiz14@gmail.com>
2024-09-26 12:29:01 -0600

    se agregan comentarios y cambios para revisión de codigos

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,24 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
-const $l = document.querySelector('.location');
+const name = document.querySelector('.name');
+const blog = document.querySelector('.blog');
+const location = document.querySelector('.location');
+//modifiqué el nombre de las constantes porque tenían carácteres especialess
 
-function displayUser(username) {
-  $n.textContent = 'cargando...';
+async function displayUser(username) {//se marcaba error en el await, la solución que encontré fue agregar el async
+  name.textContent = 'cargando...';
   const response = await fetch(`${usersEndpoint}/${username}`);
+  const data = await response.json();//se agrega el uso de response porque marcaba que no estaba siendo usado
   console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  name.textContent = `${data.name}`;
+  blog.textContent = `${data.blog}`;
+  location.textContent = `${data.location}`;//se añade uso de baltics
 }
 
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  n.textContent = `Algo salió mal: ${name}`
 }
 
 displayUser('stolinski').catch(handleError);
